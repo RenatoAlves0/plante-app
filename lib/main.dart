36ebdb1e895a00a824c84ea354e9a441be327417
renatoalves0 <renatoalves97.123@gmail.com>
2019-03-13 09:33:36 -0300

    App plante

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Plante", home: Home()));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -10,53 +10,152 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _plantas = 0;
-  String _tipoPlanta = "Cacto";
-
-  void _changePlantas(int a) {
+  List<Color> _cores = [
+    Colors.green,
+    Color.fromARGB(255, 25, 112, 182),
+    Colors.brown,
+    Colors.orange
+  ];
+  List<String> _titulos = ["Planta", "Clima", "Solo", "Luz"];
+  int _itemSelected = 0;
+  void _cahngeItemSelected(int index) {
     setState(() {
-      _plantas += a;
-      if(_plantas < 0) _tipoPlanta = "Carnívoras";
-      else _tipoPlanta = "Cacto";
+      _itemSelected = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset( "images/cactos.jpg", fit: BoxFit.fitHeight, height: 800),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulos[_itemSelected]),
+        centerTitle: true,
+        backgroundColor: _cores[_itemSelected],
+      ),
+      body: _itemSelected == 0 ? Plantas() : null,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => _cahngeItemSelected(index),
+        currentIndex: _itemSelected,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.spa),
+            title: Text(_titulos[0]),
+            backgroundColor: _cores[0],
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.cloud_queue),
+              title: Text(_titulos[1]),
+              backgroundColor: _cores[1]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.grain),
+              title: Text(_titulos[2]),
+              backgroundColor: _cores[2]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wb_sunny),
+              title: Text(_titulos[3]),
+              backgroundColor: _cores[3]),
+        ],
+      ),
+    );
+  }
+}
 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text( "Plantas: $_plantas", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+class Plantas extends StatefulWidget {
+  @override
+  _PlantasState createState() => _PlantasState();
+}
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+class _PlantasState extends State<Plantas> {
+  TextEditingController especie, genero, familia = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  double _fontSize = 20, _paddingHorizontal = 30, _paddingTop = 20;
+  Color _color = Colors.green;
+
+  void _resetFields() {
+    especie.text = genero.text = familia.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          padding: EdgeInsets.only(
+              top: _paddingTop,
+              left: _paddingHorizontal,
+              right: _paddingHorizontal),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    child: Text( "+", style: TextStyle(fontSize: 40, color: Colors.white),),
-                    onPressed: () => _changePlantas(1)
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    labelText: 'Espécie',
+                    labelStyle: TextStyle(color: _color, fontSize: _fontSize),
                   ),
+                  controller: especie,
+                  validator: (value) {
+                    if (value.isEmpty) return "Informe a Espécie";
+                  },
                 ),
-
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    child: Text( "-", style: TextStyle(fontSize: 40, color: Colors.white),),
-                    onPressed: () =>_changePlantas(-1)
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    labelText: 'Gênero',
+                    labelStyle: TextStyle(color: _color, fontSize: _fontSize),
                   ),
+                  controller: genero,
+                  validator: (value) {
+                    if (value.isEmpty) return "Informe o Gênero";
+                  },
                 ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    labelText: 'Família',
+                    labelStyle: TextStyle(color: _color, fontSize: _fontSize),
+                  ),
+                  controller: familia,
+                  validator: (value) {
+                    if (value.isEmpty) return "Informe a Família";
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: _paddingTop),
+                  child: Container(
+                    height: 50,
+                    width: 1000,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          //salvar();
+                          _resetFields();
+                        }
+                      },
+                      color: _color,
+                      child: Text(
+                        "Salvar",
+                        style:
+                            TextStyle(color: Colors.white, fontSize: _fontSize),
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
-            
-            Text( "$_tipoPlanta", style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),),
-          ],
-        )
-      ],
+          )),
+
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.blue,
+      //   tooltip: 'Adicionar planta',
+      //   child: Center(
+      //     child: Icon(Icons.add),
+      //   ),
+      // ),
     );
   }
 }
