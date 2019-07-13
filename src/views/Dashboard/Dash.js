11e473b1e7367d4d3db262d375a955266910866b
renatoalves0 <renatoalves97.123@gmail.com>
2019-07-12 23:06:01 -0300

    Integração com MQTT

--- src/views/Dashboard/Dash.js
+++ src/views/Dashboard/Dash.js
@@ -4,6 +4,7 @@ import { Dimensions, StatusBar, TouchableHighlight } from 'react-native'
 import Loader from '../../components/Loader'
 import BottomMenu from '../../components/BottomMenu'
 import estilo from '../../assets/Estilo'
+import { Client, Message } from 'react-native-paho-mqtt'
 
 export default class ListLuz extends Component {
     constructor(props) {
@@ -12,7 +13,23 @@ export default class ListLuz extends Component {
         this.state = {
             loaded: false,
         }
+        this.topico_sensores = 'plante_iot_sensores(renalves.oli@gmail.com)'
+        this.topico_regador = 'plante_iot_regador(renalves.oli@gmail.com)'
+        this.myStorage = {
+            setItem: (key, item) => {
+                myStorage[key] = item;
+            },
+            getItem: (key) => myStorage[key],
+            removeItem: (key) => {
+                delete myStorage[key];
+            },
+        }
+        this.client = new Client({
+            uri: 'ws://iot.eclipse.org:80/ws', clientId: 'plante_app_id(renalves.oli@gmail.com)',
+            storage: this.myStorage
+        })
     }
+
 
     componentWillMount() {
         this.load()
@@ -23,7 +40,39 @@ export default class ListLuz extends Component {
     }
 
     async load() {
+        this.client.on('connectionLost', (responseObject) => {
+            if (responseObject.errorCode !== 0) {
+                console.log(responseObject.errorMessage);
+            }
+        })
+
+        this.client.on('messageReceived', (message) => {
+            console.log(message.payloadString);
+        })
+
+        this.client.connect()
+            .then(() => {
+                console.log('Conexão estabelecida!');
+                return this.client.subscribe(this.topico_sensores);
+            })
+            .then(() => {
+                let message = new Message('ture');
+                message.destinationName = this.topico_regador;
+                this.client.send(message);
+            })
+            .catch((responseObject) => {
+                if (responseObject.errorCode !== 0) {
+                    console.log('Conexão perdida!:' + responseObject.errorMessage);
+                }
+            })
+
         this.setState({ loaded: true })
+    }
+
+    teste() {
+        let message = new Message('false')
+        message.destinationName = this.topico_regador
+        this.client.send(message)
     }
 
     render() {
@@ -33,7 +82,7 @@ export default class ListLuz extends Component {
                 <Content>
                     {this.state.loaded ? null : <Loader />}
                     <Row style={{ justifyContent: 'center' }} >
-                        <TouchableHighlight style={this.estilo.item_dash}>
+                        <TouchableHighlight style={this.estilo.item_dash} onPress={() => this.teste()}>
                             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
                                 <Icon name='water' type='MaterialCommunityIcons' style={{ color: this.estilo.cor.blue + 'aa', fontSize: 50 }} />
                                 <Text style={{ fontSize: 20 }} >50 %</Text>
