231406f08080d7997c597b5ed1434a8c0b27baa5
renatoalves0 <renatoalves97.123@gmail.com>
2019-07-18 18:41:37 -0300

    Integração com react-native-fs

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,6 +4,7 @@ import estilo from '../assets/Estilo'
 import LinearGradient from 'react-native-linear-gradient'
 import FeatherIcon from 'react-native-vector-icons/Feather'
 import axios from 'axios'
+import rnfs from 'react-native-fs'
 
 export default class Card extends Component {
     constructor(props) {
@@ -20,6 +21,7 @@ export default class Card extends Component {
             // { icon: 'droplet', cor1: this.estilo.cor.blue_solid, cor2: this.estilo.cor.greenish },
             // { icon: 'wind', cor1: this.estilo.cor.green_solid, cor2: this.estilo.cor.greenish }
         ]
+        this.path_weather = rnfs.DocumentDirectoryPath + '/weather.json'
     }
 
     componentWillMount() {
@@ -33,69 +35,302 @@ export default class Card extends Component {
     async load() { }
 
     async getWeather() {
-        await axios
-            // .get('https://api.darksky.net/forecast/cd6497bd71117cbb49fd4f70a5e9dc93/-4.56167,-37.769718')
-            .get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/38025?apikey=AJ8uokBYThYFdXod4T6hebp4pLvEUQom&language=pt-br&details=true&metric=true')
-            .then(async (data) => {
-                let array = data.data.DailyForecasts
-                console.log(array)
-                let sensacao_termica, sensacao_termica_sombra,
-                    temperatura, dia, noite, dia_semana, obj, array_obj = []
+        // await axios
+        //     // .get('https://api.darksky.net/forecast/cd6497bd71117cbb49fd4f70a5e9dc93/-4.56167,-37.769718')
+        //     .get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/38025?apikey=AJ8uokBYThYFdXod4T6hebp4pLvEUQom&language=pt-br&details=true&metric=true')
+        //     .then(async (data) => {
+        //         let array = data.data.DailyForecasts
+        //         let sensacao_termica, sensacao_termica_sombra,
+        //             temperatura, dia, noite, dia_semana, obj, array_obj = []
 
-                await array.forEach((element, index) => {
-                    dia_semana = element.Date
-                    sensacao_termica = {
-                        min: element.RealFeelTemperature.Minimum.Value,
-                        max: element.RealFeelTemperature.Maximum.Value
+        //         await array.forEach((element, index) => {
+        //             dia_semana = element.Date
+        //             sensacao_termica = {
+        //                 min: element.RealFeelTemperature.Minimum.Value,
+        //                 max: element.RealFeelTemperature.Maximum.Value
+        //             }
+        //             sensacao_termica_sombra = {
+        //                 min: element.RealFeelTemperatureShade.Minimum.Value,
+        //                 max: element.RealFeelTemperatureShade.Maximum.Value
+        //             }
+        //             temperatura = {
+        //                 min: element.Temperature.Minimum.Value,
+        //                 max: element.Temperature.Maximum.Value
+        //             }
+        //             dia = {
+        //                 nuvens: element.Day.CloudCover, //%
+        //                 ceu: element.Day.ShortPhrase,
+        //                 chuva_probabilidade: element.Day.RainProbability, //%
+        //                 chuva_mm: element.Day.Rain.Value,
+        //                 vento_velocidade: element.Day.Wind.Speed.Value, //km/h
+        //                 vento_direcao: element.Day.Wind.Direction.Localized,
+        //                 sol: {
+        //                     nascer: element.Sun.Rise.substring(11, 16),
+        //                     por: element.Sun.Set.substring(11, 16),
+        //                     duracao: element.HoursOfSun
+        //                 }
+        //             }
+        //             noite = {
+        //                 nuvens: element.Night.CloudCover, //%
+        //                 ceu: element.Night.ShortPhrase,
+        //                 chuva_probabilidade: element.Night.RainProbability, //%
+        //                 chuva_mm: element.Night.Rain.Value,
+        //                 vento_velocidade: element.Night.Wind.Speed.Value, //km/h
+        //                 vento_direcao: element.Night.Wind.Direction.Localized,
+        //                 lua: {
+        //                     nascer: element.Moon.Rise.substring(11, 16),
+        //                     por: element.Moon.Set.substring(11, 16)
+        //                 }
+        //             }
+        //             obj = {
+        //                 id: index,
+        //                 dia_semana: this.getDayOfWeek(dia_semana),
+        //                 sensacao_termica: sensacao_termica,
+        //                 sensacao_termica_sombra: sensacao_termica_sombra,
+        //                 temperatura: temperatura,
+        //                 dia: dia,
+        //                 noite: noite
+        //             }
+        //             array_obj.push(obj)
+        //         })
+        let array_obj = await [
+            {
+                "id": 0,
+                "dia_semana": "Quinta",
+                "sensacao_termica": {
+                    "min": 20.5,
+                    "max": 33.7
+                },
+                "sensacao_termica_sombra": {
+                    "min": 20.5,
+                    "max": 31
+                },
+                "temperatura": {
+                    "min": 21.7,
+                    "max": 32.4
+                },
+                "dia": {
+                    "nuvens": 73,
+                    "ceu": "Sol entre nuvens",
+                    "chuva_probabilidade": 2,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 18.5,
+                    "vento_direcao": "SE",
+                    "sol": {
+                        "nascer": "05:41",
+                        "por": "17:34",
+                        "duracao": 4.5
                     }
-                    sensacao_termica_sombra = {
-                        min: element.RealFeelTemperatureShade.Minimum.Value,
-                        max: element.RealFeelTemperatureShade.Maximum.Value
+                },
+                "noite": {
+                    "nuvens": 42,
+                    "ceu": "Parcialmente nublado",
+                    "chuva_probabilidade": 2,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 16.7,
+                    "vento_direcao": "SE",
+                    "lua": {
+                        "nascer": "19:07",
+                        "por": "07:41"
                     }
-                    temperatura = {
-                        min: element.Temperature.Minimum.Value,
-                        max: element.Temperature.Maximum.Value
+                }
+            },
+            {
+                "id": 1,
+                "dia_semana": "Sexta",
+                "sensacao_termica": {
+                    "min": 23.4,
+                    "max": 33.8
+                },
+                "sensacao_termica_sombra": {
+                    "min": 23.4,
+                    "max": 30.6
+                },
+                "temperatura": {
+                    "min": 23.3,
+                    "max": 31.4
+                },
+                "dia": {
+                    "nuvens": 4,
+                    "ceu": "Ensolarado",
+                    "chuva_probabilidade": 1,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 20.4,
+                    "vento_direcao": "SE",
+                    "sol": {
+                        "nascer": "05:41",
+                        "por": "17:34",
+                        "duracao": 11.3
                     }
-                    dia = {
-                        nuvens: element.Day.CloudCover, //%
-                        ceu: element.Day.ShortPhrase,
-                        chuva_probabilidade: element.Day.RainProbability, //%
-                        chuva_mm: element.Day.Rain.Value,
-                        vento_velocidade: element.Day.Wind.Speed.Value, //km/h
-                        vento_direcao: element.Day.Wind.Direction.Localized,
-                        sol: {
-                            nascer: element.Sun.Rise.substring(11, 16),
-                            por: element.Sun.Set.substring(11, 16),
-                            duracao: element.HoursOfSun
-                        }
+                },
+                "noite": {
+                    "nuvens": 10,
+                    "ceu": "Céu claro",
+                    "chuva_probabilidade": 4,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 20.4,
+                    "vento_direcao": "SE",
+                    "lua": {
+                        "nascer": "19:54",
+                        "por": "08:23"
                     }
-                    noite = {
-                        nuvens: element.Night.CloudCover, //%
-                        ceu: element.Night.ShortPhrase,
-                        chuva_probabilidade: element.Night.RainProbability, //%
-                        chuva_mm: element.Night.Rain.Value,
-                        vento_velocidade: element.Night.Wind.Speed.Value, //km/h
-                        vento_direcao: element.Night.Wind.Direction.Localized,
-                        lua: {
-                            nascer: element.Moon.Rise.substring(11, 16),
-                            por: element.Moon.Set.substring(11, 16)
-                        }
+                }
+            },
+            {
+                "id": 2,
+                "dia_semana": "Sábado",
+                "sensacao_termica": {
+                    "min": 23.2,
+                    "max": 33.3
+                },
+                "sensacao_termica_sombra": {
+                    "min": 23.2,
+                    "max": 30.3
+                },
+                "temperatura": {
+                    "min": 23.1,
+                    "max": 30
+                },
+                "dia": {
+                    "nuvens": 13,
+                    "ceu": "Predominantemente ensolarado",
+                    "chuva_probabilidade": 9,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 22.2,
+                    "vento_direcao": "SE",
+                    "sol": {
+                        "nascer": "05:41",
+                        "por": "17:34",
+                        "duracao": 10.5
                     }
-                    obj = {
-                        id: index,
-                        dia_semana: this.getDayOfWeek(dia_semana),
-                        sensacao_termica: sensacao_termica,
-                        sensacao_termica_sombra: sensacao_termica_sombra,
-                        temperatura: temperatura,
-                        dia: dia,
-                        noite: noite
+                },
+                "noite": {
+                    "nuvens": 34,
+                    "ceu": "Parcialmente nublado",
+                    "chuva_probabilidade": 25,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 16.7,
+                    "vento_direcao": "SE",
+                    "lua": {
+                        "nascer": "20:38",
+                        "por": "09:05"
                     }
-                    array_obj.push(obj)
-                })
-                this.setState({ lista_weather: array_obj })
-                console.log(array_obj)
+                }
+            },
+            {
+                "id": 3,
+                "dia_semana": "Domingo",
+                "sensacao_termica": {
+                    "min": 22.8,
+                    "max": 32.8
+                },
+                "sensacao_termica_sombra": {
+                    "min": 22.8,
+                    "max": 29.9
+                },
+                "temperatura": {
+                    "min": 22.9,
+                    "max": 30.1
+                },
+                "dia": {
+                    "nuvens": 54,
+                    "ceu": "Predominantemente nublado",
+                    "chuva_probabilidade": 25,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 20.4,
+                    "vento_direcao": "SE",
+                    "sol": {
+                        "nascer": "05:41",
+                        "por": "17:34",
+                        "duracao": 6.9
+                    }
+                },
+                "noite": {
+                    "nuvens": 13,
+                    "ceu": "Predominantemente claro",
+                    "chuva_probabilidade": 10,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 16.7,
+                    "vento_direcao": "SE",
+                    "lua": {
+                        "nascer": "21:22",
+                        "por": "09:45"
+                    }
+                }
+            },
+            {
+                "id": 4,
+                "dia_semana": "Segunda",
+                "sensacao_termica": {
+                    "min": 22.7,
+                    "max": 31.6
+                },
+                "sensacao_termica_sombra": {
+                    "min": 22.7,
+                    "max": 28.4
+                },
+                "temperatura": {
+                    "min": 22.8,
+                    "max": 29.4
+                },
+                "dia": {
+                    "nuvens": 23,
+                    "ceu": "Predominantemente ensolarado",
+                    "chuva_probabilidade": 5,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 20.4,
+                    "vento_direcao": "SE",
+                    "sol": {
+                        "nascer": "05:41",
+                        "por": "17:34",
+                        "duracao": 9.2
+                    }
+                },
+                "noite": {
+                    "nuvens": 11,
+                    "ceu": "Predominantemente claro",
+                    "chuva_probabilidade": 9,
+                    "chuva_mm": 0,
+                    "vento_velocidade": 16.7,
+                    "vento_direcao": "SE",
+                    "lua": {
+                        "nascer": "22:05",
+                        "por": "10:25"
+                    }
+                }
+            }
+        ]
+        console.log(array_obj)
+        await rnfs.writeFile(this.path_weather, JSON.stringify(array_obj), 'utf8')
+            .then(() => {
+                console.log('Weather gravado com sucesso no armazenamento interno!')
             })
-            .catch((erro) => { console.error(erro) })
+            .catch((err) => {
+                console.log('Falha ao gravar Weather no armazenamento interno!')
+                console.log(err.message)
+            })
+
+        await rnfs.readDir(rnfs.DocumentDirectoryPath)
+            .then((result) => {
+                console.log('Resultado de leitura obtido', result);
+                return Promise.all([rnfs.stat(result[0].path), result[0].path]);
+            })
+            .then((statResult) => {
+                if (statResult[0].isFile()) {
+                    return rnfs.readFile(statResult[1], 'utf8')
+                }
+                return 'no file'
+            })
+            .then((contents) => {
+                this.setState({ lista_weather: JSON.parse(contents) })
+                console.log(this.state.lista_weather)
+            })
+            .catch((err) => {
+                console.log(err.message, err.code)
+            })
+
+        // })
+        //         .catch((erro) => { console.error(erro) })
     }
 
     getDayOfWeek(date) {
