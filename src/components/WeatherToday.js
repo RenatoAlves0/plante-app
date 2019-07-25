49ccaec25de3b38bf593ebfc295fe44293ac9836
renatoalves0 <renatoalves97.123@gmail.com>
2019-07-25 16:47:41 -0300

    base para nova biblioteca de gráficos

--- src/components/WeatherToday.js
+++ src/components/WeatherToday.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Dimensions, ScrollView, Modal } from 'react-native'
-import { Text, Button, View, Form, ListItem, Row, Col, Spinner, Content, Container } from 'native-base'
+import { Dimensions, ScrollView, Modal, View } from 'react-native'
+import { Button, Form, ListItem, Row, Col, Spinner, Content, Container, Text } from 'native-base'
 import estilo from '../assets/Estilo'
 import LinearGradient from 'react-native-linear-gradient'
 import FeatherIcon from 'react-native-vector-icons/Feather'
 import axios from 'axios'
 import rnfs from 'react-native-fs'
-import { LineChart } from 'react-native-chart-kit'
+import { Grid, AreaChart, XAxis, YAxis } from 'react-native-svg-charts'
+import { G, Line, Circle, Path, Svg } from 'react-native-svg'
+import * as shape from 'd3-shape'
 
 export default class Card extends Component {
     constructor(props) {
@@ -31,46 +33,25 @@ export default class Card extends Component {
     async load() {
     }
 
-    renderTemperatura() {
-        return <LineChart
-            data={{
-                labels: ['1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', '11h', '12h'],
-                datasets: [{
-                    data: [
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                    ]
-                }]
-            }}
-            width={Dimensions.get('window').width * 1.5} // from react-native
-            height={320}
-            yAxisLabel={'$'}
-            chartConfig={{
-                backgroundColor: '#e26a00',
-                backgroundGradientFrom: '#fb8c00',
-                backgroundGradientTo: '#ffa726',
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {}
-            }}
-            bezier
-            style={{
-                borderRadius: 10
-            }}
-        />
-    }
+    // renderTemperatura() {
+    //     return
+    // }
 
     render() {
+        const data = [24, 27, 30, 31, 30, 33, 34, 29, 28, 30, 33, 29]
+        const data1 = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 0]
+        const axesSvg = { fontSize: 18, fill: this.estilo.cor.black };
+        const xAxisHeight = 30
+        const Decorator = ({ x, y, data }) => {
+            return data.map((value, index) => (
+                <Svg key={index} translateX={x(index)} translateY={y(value)} strokeWidth={50}>
+                    <Text style={{ marginTop: -22, marginLeft: -8 }}>{value}º</Text>
+                </Svg>
+            ))
+        }
+        const Line = ({ line }) => (
+            <Path d={line} stroke={this.estilo.cor.gray_white} fill={'none'} strokeWidth={3} />
+        )
         return (
             <LinearGradient colors={[this.card_weather[this.state.card_weather_atual].cor1,
             this.card_weather[this.state.card_weather_atual].cor2]} useAngle={true}
@@ -88,8 +69,37 @@ export default class Card extends Component {
                         </Button>
                     ))}
                 </Form>
-                <ScrollView horizontal={true} style={{ margin: 15, marginTop: 0, borderRadius: 10 }}>
-                    {this.renderTemperatura()}
+                <ScrollView horizontal={true} style={{ margin: 15, marginTop: 0, borderRadius: 10, backgroundColor: 'white' }}>
+                    {/* {this.renderTemperatura()} */}
+                    <View style={{ padding: 20, flexDirection: 'row', width: Dimensions.get('window').width * 1.7 }}>
+                        {/* <YAxis
+                            data={data}
+                            style={{ marginBottom: xAxisHeight }}
+                            svg={axesSvg}
+                        /> */}
+                        <View style={{ flex: 1, marginLeft: 10 }}>
+                            <AreaChart
+                                style={{ height: 150 }}
+                                data={data}
+                                svg={{ fill: this.estilo.cor.gray_translucid }}
+                                curve={shape.curveNatural}
+                                contentInset={{left: 10, right: 20}}
+                                yMin={0}
+                                yMax={50}
+                            >
+                                <Line />
+                                <Decorator />
+                            </AreaChart>
+                            <XAxis
+                                style={{ marginHorizontal: -10, height: xAxisHeight, paddingTop: 15 }}
+                                data={data}
+                                formatLabel={index => data1[index]+'h'}
+                                contentInset={{ left: 20, right: 30 }}
+                                svg={axesSvg}
+                                numberOfTicks={12}
+                            />
+                        </View>
+                    </View>
                 </ScrollView>
                 {/* {this.state.loaded ? null : <Spinner color={this.estilo.cor.white + '77'} style={{ alignSelf: 'center', marginBottom: 30 }} />} */}
             </LinearGradient >
